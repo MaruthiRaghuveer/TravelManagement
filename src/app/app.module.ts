1ef6b1933dc01539d6f0b2e5a889196d86dd508a
MaruthiRaghuveer <raghuveer.pm@gmail.com>
2018-09-17 15:20:52 +0530

    committed again

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DataTablesModule } from 'angular-datatables';
+import { AppComponent } from './app.component';
+
+
+import { TravelDetailsComponent } from './travel-details/travel-details.component';
+import { TravelRequestComponent } from './travel-request/travel-request.component';
+
+const AppRoutes = [
+  {path: 'app-travel-details', component: TravelDetailsComponent},
+  {path: 'app-travel-request', component: TravelRequestComponent},
+  {path: '', component: TravelDetailsComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TravelDetailsComponent,
+    TravelRequestComponent
+  ],
+  imports: [
+    BrowserModule,
+    DataTablesModule,
+    RouterModule.forRoot(AppRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
